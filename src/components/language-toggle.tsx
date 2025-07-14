c88eb319b8e5ba583a7464cd76a250277290fa27
Pand <pandemoniuzz1@gmail.com>
2025-07-14 12:49:58 +0000

    no me guta has que solo ocupe el tamos del ES Y EN admea quita el > que

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -17,11 +17,17 @@ export function LanguageToggle() {
 
     return (
         <Select onValueChange={(value: 'es' | 'en') => setLanguage(value)} defaultValue={language}>
-            <SelectTrigger className={cn(buttonVariants({ variant: "outline", size: "icon" }))}>
+            <SelectTrigger 
+              className={cn(
+                buttonVariants({ variant: "outline", size: "icon" }),
+                "w-10 h-10 p-0"
+              )}
+              hideChevron={true}
+            >
                   <Globe className="h-[1.2rem] w-[1.2rem]" />
                   <span className="sr-only">Toggle language</span>
             </SelectTrigger>
-            <SelectContent align="end">
+            <SelectContent align="end" className="min-w-0 w-auto">
                 <SelectItem value="es">Es</SelectItem>
                 <SelectItem value="en">En</SelectItem>
             </SelectContent>
