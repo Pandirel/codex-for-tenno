8176d845ff02185cb067f3dfdfdae92a078f11e2
Pand <pandemoniuzz1@gmail.com>
2025-09-17 10:19:01 +0000

    en el select del boton de cambio de idioma tambien hay un hover quitalo

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -20,7 +20,7 @@ export function LanguageToggle() {
             <SelectTrigger 
               className={cn(
                 buttonVariants({ variant: "outline", size: "icon" }),
-                "w-10 h-10 p-0"
+                "w-10 h-10 p-0 hover:bg-transparent"
               )}
               hideChevron={true}
             >
