1824ab8d9de531a301cb4632b38e653f99514853
Pand <pandemoniuzz1@gmail.com>
2025-09-17 10:17:37 +0000

    Espera si hay errores quita todos los hover de los botones de la pagina

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,15 +9,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-gradient-to-r from-primary/80 to-accent/80 text-primary-foreground hover:bg-gradient-to-r hover:from-primary hover:to-destructive",
+        default: "bg-gradient-to-r from-primary/80 to-accent/80 text-primary-foreground",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground",
+        ghost: "",
+        link: "text-primary underline-offset-4",
       },
       size: {
         default: "h-10 px-4 py-2",
